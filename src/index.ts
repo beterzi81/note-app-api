31f8db7eb91a67a94aa1a6c2e09d090f756c93cd
Burak Emre Terzi <beterzi81@gmail.com>
2024-07-26 09:10:54 +0300

    chore: Add CRUD API endpoints for notes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,53 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 const pool = require('./db');
 
 const app = express();
+const prisma = new PrismaClient();
 const port = process.env.PORT || 5000;
 app.use(express.json())
 app.use(cors())
 
 app.get("/api/notes" , async(req,res) =>{
-    res.json({ message: "Hello World"})
+    const notes = await prisma.note.findMany()
+    res.json({notes})
+})
+
+app.post("/api/notes", async(req,res) =>{
+    const {title, content} = req.body
+    const note = await prisma.note.create({
+        data:{
+            title,
+            content
+        }
+    })
+    res.json({note})
+})
+
+app.put("/api/notes/:id", async(req,res) =>{
+    const {id} = req.params
+    const {title, content} = req.body
+    const note = await prisma.note.update({
+        where:{
+            id: parseInt(id)
+        },
+        data:{
+            title,
+            content
+        }
+    })
+    res.json({note})
+})
+
+app.delete("/api/notes/:id", async(req,res) =>{
+    const {id} = req.params
+    const note = await prisma.note.delete({
+        where:{
+            id: parseInt(id)
+        }
+    })
+    res.json({message: "Note deleted"})
 })
 
 app.get('/api/test', async (req, res) => {
